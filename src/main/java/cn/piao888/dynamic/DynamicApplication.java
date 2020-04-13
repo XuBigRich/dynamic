ad47a0caad08b8f0025bf09aa343bec43ae3db81
SDGC <hongzhi.xu@SDGC.com>
2020-04-13 17:56:49 +0800

    援引出处

--- src/main/java/cn/piao888/dynamic/DynamicApplication.java
+++ src/main/java/cn/piao888/dynamic/DynamicApplication.java
@@ -3,7 +3,8 @@ package cn.piao888.dynamic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+//https://blog.csdn.net/u012881904/article/details/77449710?depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromBaidu-2&utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromBaidu-2
+//参考文章
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class DynamicApplication {
 
